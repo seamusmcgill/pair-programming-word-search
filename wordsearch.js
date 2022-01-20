89ba7dbb951ecd07c7a7f22b683f4d2173288abc
Seamus McGill <seamusmcgill@gmail.com>
2022-01-19 19:22:00 -0500

    Built a word search program

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!letters || !word) {
+    return false;
+  }
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  const verticalJoin = [];
+  for (let i = 0; i < letters.length; i++) {
+    let verticalWord = "";
+    for (let j = 0; j < letters[0].length; j++) {
+      if (letters[j][i]) {
+        verticalWord += letters[j][i];
+      }
+    }
+    if (verticalWord) {
+      verticalJoin.push(verticalWord);
+    }
+  }
+  for (const v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
